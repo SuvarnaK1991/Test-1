c7823a9c7fdf0a4589c0e7bfd65d10c2da984922
SuvarnaK1991 <88269377+SuvarnaK1991@users.noreply.github.com>
2021-08-01 16:42:53 +0530

    Update JSTest1.js

--- JSTest1.js
+++ JSTest1.js
@@ -2,6 +2,8 @@
 <head>This is a sample JS</head>
 <body>
 <p id="para1">Change me by clicking on the button</p>
+<p id="para2">Change me too by clicking on the second button</p>
 <button type="button" onclick="document.getElementById('para1').innerHTML='I am changed!!!'"></button>
+<button type="button" onclick="document.getElementById('para2').innerHTML='I am also changed!!!'"></button>
 </body>
 </html>
