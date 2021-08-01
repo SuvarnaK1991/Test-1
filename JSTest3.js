4b425968ce0722fad543702748e8853f959a4b11
SuvarnaK1991 <suvarna.achu@gmail.com>
2021-08-01 17:22:01 +0530

    Third git update

---
+++ JSTest3.js
@@ -0,0 +1,9 @@
+<html>
+    <head>
+        This is another trial js
+    </head>
+    <body>
+        <button type="button" onclick="document.getElementById().innerHTML=Am changed!">
+            click to change my content</button>
+    </body>
+</html>
