99b75b07bba6398ba5c2a72d45c11aa994f1539c
K <suvarna.k@hp.com>
2021-08-01 16:22:48 +0530

    first trial code

---
+++ JSTest1.js
@@ -0,0 +1,7 @@
+<html>
+<head>This is a sample JS</head>
+<body>
+<p id="para1">Change me by clicking on the button</p>
+<button type="button" onclick="document.getElementById('para1').innerHTML='I am changed!!!'"></button>
+</body>
+</html>
